982ef5d8b36b81823e4dd0792345916ebfbbdbd0
GowthamiGK <gowthami6793@gmail.com>
2015-06-21 17:34:39 +0530

    First check in

---
+++ NotesApp/gen/com/example/artooandroidproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.artooandroidproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ NotesApp/src/com/example/artooandroidproject/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.artooandroidproject;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    	
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        
+        else if(id == R.id.action_search){
+        	Context context = getApplicationContext();
+        	CharSequence text = "Clicked Search";
+        	int duration = Toast.LENGTH_LONG;
+
+        	Toast toast = Toast.makeText(context, text, duration);
+        	toast.show();
+        	return true;
+        }
+        else if(id == R.id.action_New){
+        	/*Context context = getApplicationContext();
+        	CharSequence text = "Clicked New";
+        	int duration = Toast.LENGTH_SHORT;
+
+        	Toast toast = Toast.makeText(context, text, duration);
+        	toast.show();*/
+        	Intent intent = new Intent(this, ActivityNew.class);
+            
+            startActivity(intent);
+        	return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
